8c3fb993ad5af1075a5e692ed4bf5b5b234eadcb
qq292 <2920007919@qq.com>
2023-01-04 20:48:50 +0800

    优化框架

--- TS.lua
+++ TS.lua
@@ -1,33 +1,27 @@
-Setting = {
-    tapLag=500--点击后延时
-    
+etting = {
+    tapLag = 500--点击后延时
+
 }
 
+local Factor = {
+    x = -1,
+    y = -1,
+    position = {},
+    factor = function(self, base, feature)
+        self:__isAnd(base, feature)--找色
 
-local Factor ={
-    x=-1,
-    y=-1,
-    position={},
-    factor=function (self,base, feature)
-        self:__isAnd(base,feature)--找色
-        
         self:__isTap(feature)--是否点击
         self:__isAgain(feature)--全部找到再执行下一个
 
-
         self:__findColor_call(feature)--找到颜色回调
-            
-    
+
         self:__nuFindColor_call(feature)--未找到颜色回调
 
-        
-        
         return self
     end,
-    
 
     --全部找到再执行下一个
-    __isAgain=function (self,feature)
+    __isAgain = function(self, feature)
         if feature.____Set_isAgain and self.x ~= -1 then
             self:__isAnd(feature)
             self:__isTap(feature)--是否点击
@@ -37,32 +31,32 @@ local Factor ={
         end
     end,
     --未找到颜色回调
-    __nuFindColor_call=function (self,feature)
-        if self.x==-1 and feature.____Set_unFindColor_call ~= nil then
+    __nuFindColor_call = function(self, feature)
+        if self.x == -1 and feature.____Set_unFindColor_call ~= nil then
             feature.____Set_unFindColor_call(feature)
         end
     end
-    ,
+,
     --找到颜色回调
-    __findColor_call=function (self,feature)
-        if self.x~=-1 and feature.____Set_findColor_call ~= nil then
-            feature.____Set_findColor_call(feature, self.x, self.y,self.position)
+    __findColor_call = function(self, feature)
+        if self.x ~= -1 and feature.____Set_findColor_call ~= nil then
+            feature.____Set_findColor_call(feature, self.x, self.y, self.position)
         end
     end
-    ,
+,
     --是否点击
-    __isTap=function (self,feature)
-        if self.x~=-1 and feature.____Set_isTap then
-            feature:click(self.x+feature.____Set_shifting_x, self.y+feature.____Set_shifting_y)
+    __isTap = function(self, feature)
+        if self.x ~= -1 and feature.____Set_isTap then
+            feature:click(self.x + feature.____Set_shifting_x, self.y + feature.____Set_shifting_y)
         end
     end,
-    
+
     --找色
-    __isAnd=function (self,base,feature)
+    __isAnd = function(self, base, feature)
         if feature.____Set_isAnd == Feature.ALL then
             self.position = feature:findColor_all()
-            if #self.position ~=0 then
-                self.x, self.y =  self.position[1].x,self.position[1].y
+            if #self.position ~= 0 then
+                self.x, self.y = self.position[1].x, self.position[1].y
             end
         elseif feature.____Set_isAnd == Feature.AND then
             self.x, self.y = feature:findColor_and()
@@ -73,25 +67,20 @@ local Factor ={
     end
 }
 
-
-
-
-Base = setmetatable({
+local Base = {
     flag = false,
-    procedure_list=nil,
-    -- 子类重写
+    procedure_list = nil,
     procedure = function(self)
         error("error:  没有找到 procedure()")
     end,
-    -- 子类重写
     log = function(feature, x, y)
 
     end,
     ui = function(self)
-        
+
     end,
-    __run = function(self,procedure_list)
-       
+    __run = function(self, procedure_list)
+
         for _, feature in ipairs(procedure_list) do
             if self.flag then
                 return
@@ -99,27 +88,27 @@ Base = setmetatable({
             if type(feature) == "function" then
                 feature()
             else
-                local factor= Factor:factor(self, feature)
+                local factor = Factor:factor(self, feature)
                 local sub_feature = feature.____Set_sub_feature
                 if factor.x ~= -1 then
-                    
-                    if #sub_feature ~= 0  then
+
+                    if #sub_feature ~= 0 then
                         self:__run(sub_feature)
                     end
                     if feature.____Set_findBreak then
                         break
                     end
-                    
-                    
+
+
                 end
             end
-            
+
         end
         self:__run(self.procedure_list)
     end,
-    start=function (self)
+    start = function(self)
         if not self.flag then
-            if self.procedure_list==nil then
+            if self.procedure_list == nil then
                 self.procedure_list = self:procedure()
             end
             self:__run(self.procedure_list)
@@ -129,76 +118,49 @@ Base = setmetatable({
         self.flag = true
     end
 
-}, {
-    __call = function(self)
-        error("不能实例化")
-    end
-})
-
-
-ColorBase = {
-    
-    findMultiColorInRegionFuzzy = function()
-        return findMultiColorInRegionFuzzy
-    end,
-    tap = function(self)
-        return tap
-    end,
-    randomTap = function(self)
-        return randomTap
-    end,
-    sleep=function ()
-        return mSleep
-    end,
-    findMultiColorInRegionFuzzyExt=function ()
-        return findMultiColorInRegionFuzzyExt
-    end
-    
-
 }
 
-
-Color = setmetatable({
-    __findColor = function(self, t,isAll)
-        if #t==0 then
+Color = {
+    __findColor = function(self, t, isAll)
+        if #t == 0 then
             if isAll then
                 return {}
             else
-                return -1,-1
+                return -1, -1
             end
         end
         isAll = isAll or false
-        t[8] = t[8] or {orient = 2}
+        t[8] = t[8] or { orient = 2 }
         if isAll then
-            return self:findMultiColorInRegionFuzzyExt()(t[1], t[2], t[3], t[4], t[5], t[6], t[7])
+            return findMultiColorInRegionFuzzyExt(t[1], t[2], t[3], t[4], t[5], t[6], t[7])
         else
-            return self:findMultiColorInRegionFuzzy()(t[1], t[2], t[3], t[4], t[5], t[6], t[7],t[8])
+            return findMultiColorInRegionFuzzy(t[1], t[2], t[3], t[4], t[5], t[6], t[7], t[8])
         end
-        
+
     end,
- 
-    click = function(self, x, y,t,r,pic)
+
+    click = function(self, x, y, t, r, pic)
         t = t or self.____Set_tapLag or 500
         r = r or self.____Set_tapRandomRange or 5
-        if pic~=nil then
-            self:randomTap()(x, y,r,pic)
+        if pic ~= nil then
+            randomTap(x, y, r, pic)
         else
-            self:randomTap()(x, y,r)
+            randomTap(x, y, r)
         end
-        self:sleep()(t)
+        mSleep(t)
     end,
-    findColor_all=function (self)
+    findColor_all = function(self)
         local color_list = self.____color_list
-        local position={}
+        local position = {}
         if #color_list ~= 0 then
             for _, color in pairs(color_list) do
 
-                local ipos = self:__findColor(color,true)
+                local ipos = self:__findColor(color, true)
                 if #ipos ~= 0 then
                     for _, pos in pairs(ipos) do
-                        table.insert(position,pos)
+                        table.insert(position, pos)
                     end
-                    
+
                 end
             end
         end
@@ -241,23 +203,14 @@ Color = setmetatable({
         return x, y
     end
 
-}, {
-
-    __call = function(self, main, sub, z, x, y, x2, y2, tab)
-        self.main, self.sub, self.z, self.x, self.y, self.x2, self.y2, self.tab = main, sub, z, x, y, x2, y2, tab
-        return setmetatable({}, {
-            __index = self
-        })
-    end,
-    __index = ColorBase
-})
+}
 
 -- 脚本控制类
 Feature = setmetatable({
-    _TYPE_="Feature",
-    ALL=0,
-    AND=1,
-    OR=3,
+    _TYPE_ = "Feature",
+    ALL = 0,
+    AND = 1,
+    OR = 3,
     ____Set_tapLag = Setting.tapLag,
     ____Set_isTap = false,
     ____Set_findColor_call = nil,
@@ -265,33 +218,33 @@ Feature = setmetatable({
     ____Set_sub_feature = {},
     ____Set_isAnd = 1,
     ____Set_tag = nil,
-    ____Set_shifting_x=0,
-    ____Set_shifting_y=0,
+    ____Set_shifting_x = 0,
+    ____Set_shifting_y = 0,
     ____Set_isAgain = false,
-    ____Set_tapRandomRange =5,
-    ____Set_findBreak =false,
+    ____Set_tapRandomRange = 5,
+    ____Set_findBreak = false,
 
 
     --找到后跳出流程
-    set_findBreak=function (self,findBreak)
+    set_findBreak = function(self, findBreak)
         self.____Set_findBreak = findBreak
         return self
     end,
 
 
     --点击随机范围
-    set_tapRandomRange=function (self,randomRange)
+    set_tapRandomRange = function(self, randomRange)
         self.____Set_tapRandomRange = randomRange
         return self
     end,
 
     --全部点完再执行下一个
-    set_isAgain=function (self,isAgain)
+    set_isAgain = function(self, isAgain)
         self.____Set_isAgain = isAgain
         return self
     end,
     -- 点击后延迟
-    set_tapLag=function (self,tapLag)
+    set_tapLag = function(self, tapLag)
         self.____Set_tapLag = tapLag
         return self
     end,
@@ -335,14 +288,14 @@ Feature = setmetatable({
     end,
     -- 界面内子特征
     set_sub_feature = function(self, ...)
-        self.____Set_sub_feature = {...}
+        self.____Set_sub_feature = { ... }
         return self
     end
 
 }, {
 
     __call = function(self, ...)
-        return setmetatable({____color_list={...}}, {
+        return setmetatable({ ____color_list = { ... } }, {
             __index = self
         })
     end,
@@ -350,22 +303,12 @@ Feature = setmetatable({
 
 })
 
-BaseMeta={
-    __call = function(self,...)
-        return setmetatable({...}, {
-            __index = self
-        })
-
+BaseMeta = {
+    __call = function(self, ...)
+        return self
     end,
     __index = Base
 }
-
-
-
-----------------------------上面是框架部分--------------------
-----------------------------下面是示例部分--------------------
-
-
 
 
 
